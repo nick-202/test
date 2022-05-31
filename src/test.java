9c8d42174254a0612e5376bf39a53e93ea8761d6
nikolay.sergeevich <nikolay.sergeevich44@yandex.ru>
2022-05-31 19:59:47 +0300

    2 test commit

--- src/test.java
+++ src/test.java
@@ -2,5 +2,11 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("Test project" + "&" + "PullRequest GitHub");
+        mainTest(5, 17);
+    }
+    public static boolean mainTest(int a, int b) {
+        int sum = a + b;
+        boolean b1 = sum >= 10 && sum <= 20;
+        return false;
     }
 }
